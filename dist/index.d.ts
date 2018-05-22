831bbd248865c8dfa6d4be1126ba178b750bdd23
Phil Kunz <gitlab@philkunz.com>
2018-05-22 13:44:58 +0200

    fix(ci): fix ci

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="node" />
-import 'typings-global';
 import { ChildProcess } from 'child_process';
 export declare let openUrl: (urlArg: any) => Promise<ChildProcess>;
--- dist/index.js
+++ dist/index.js
@@ -8,12 +8,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("typings-global");
 const open = require("opn");
 exports.openUrl = (urlArg) => __awaiter(this, void 0, void 0, function* () {
-    const childProcess = yield open(urlArg, {
-        wait: false
-    });
-    return childProcess;
+    if (!(process.env.CI === 'true')) {
+        const childProcess = yield open(urlArg, {
+            wait: false
+        });
+        return childProcess;
+    }
+    else {
+        return null;
+    }
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90cy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUEsMEJBQXdCO0FBQ3hCLDRCQUE2QjtBQUdsQixRQUFBLE9BQU8sR0FBRyxDQUFNLE1BQU0sRUFBQyxFQUFFO0lBQ2xDLE1BQU0sWUFBWSxHQUFHLE1BQU0sSUFBSSxDQUFDLE1BQU0sRUFBRTtRQUN0QyxJQUFJLEVBQUUsS0FBSztLQUNaLENBQUMsQ0FBQztJQUNILE9BQU8sWUFBWSxDQUFDO0FBQ3RCLENBQUMsQ0FBQSxDQUFDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90cy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUEsNEJBQTZCO0FBR2xCLFFBQUEsT0FBTyxHQUFHLENBQU0sTUFBTSxFQUFDLEVBQUU7SUFDbEMsSUFBRyxDQUFDLENBQUMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxFQUFFLEtBQUssTUFBTSxDQUFDLEVBQUU7UUFDL0IsTUFBTSxZQUFZLEdBQUcsTUFBTSxJQUFJLENBQUMsTUFBTSxFQUFFO1lBQ3RDLElBQUksRUFBRSxLQUFLO1NBQ1osQ0FBQyxDQUFDO1FBQ0gsT0FBTyxZQUFZLENBQUM7S0FDckI7U0FBTTtRQUNMLE9BQU8sSUFBSSxDQUFBO0tBQ1o7QUFDSCxDQUFDLENBQUEsQ0FBQyJ9
