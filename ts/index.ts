cb1ee35f2e2afb487ddff6fb1677d5c10f458602
Phil Kunz <gitlab@philkunz.com>
2018-05-22 13:42:21 +0200

    fix(ci): better deal with ci environments

--- ts/index.ts
+++ ts/index.ts
@@ -2,8 +2,12 @@ import open = require('opn');
 import { ChildProcess } from 'child_process';
 
 export let openUrl = async urlArg => {
-  const childProcess = await open(urlArg, {
-    wait: false
-  });
-  return childProcess;
+  if(!(process.env.CI === 'true')) {
+    const childProcess = await open(urlArg, {
+      wait: false
+    });
+    return childProcess;
+  } else {
+    return null
+  }
 };
